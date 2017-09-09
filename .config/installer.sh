5264443a11dd6cdf2480726416db1e820e2e0df6
Lauren <lauren@tomasello.me>
2017-09-09 15:41:14 +0100

    Fix issues with installer.sh (hopefully)

--- .config/installer.sh
+++ .config/installer.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
 # Set-up the bare repo
-git clone --bare https://github.com/laurenkt/dotfiles.git $HOME/.dotfiles.git
+if [ -d "$HOME/.dotfiles.git" ]; then
+	echo "Directory already exists, using existing..."
+else
+	echo "Downloading from GitHub..."
+	git clone --bare https://github.com/laurenkt/dotfiles.git $HOME/.dotfiles.git
+fi
+
 function git. {
    /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $@
 }
 
 # Checkout files
 git. checkout
+
+if [ $? = 0 ]; then
+	echo "Checked out config";
+else
+	echo "Backing up pre-existing dot files...";
+	git. checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config/backup/{}
+	git. checkout
+fi
 
 # This must go after so config exists
 git. config status.showUntrackedFiles no
@@ -20,7 +34,7 @@ else
 	echo "No zsh found. Installing..."
 
 	if command -v brew >/dev/null 2>&1; then
-		echo "Homebrew found. Using..."
+		echo "Homebrew found. Installing zsh..."
 		brew install zsh
 	else
 		echo "Trying apt..."
@@ -28,9 +42,12 @@ else
 	fi
 fi
 
-if command -v chsh >/dev/null 2>&1; then
-	chsh -s $(command -v zsh)
-fi
-
-zsh
+if [[ "$SHELL" != *zsh ]]; then
+	echo "👌 You're all set!"
+else
+	echo 'zsh is installed but you should still change your default shell to zsh'
+	if command -v chsh >/dev/null 2>&1; then
+		echo '> chsh -s $(command -v zsh)'
+	fi
+	zsh; exit;
 exit

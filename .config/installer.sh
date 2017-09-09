a9b478db57e80675a18c951993efe10b0220e2f0
Lauren <lauren@tomasello.me>
2017-09-09 11:31:40 +0100

    Add zsh autoinstaller

--- .config/installer.sh
+++ .config/installer.sh
@@ -6,5 +6,31 @@ function git. {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
-git. config status.showUntrackedFiles no
+# Checkout files
 git. checkout
+
+# This must go after so config exists
+git. config status.showUntrackedFiles no
+
+# If zsh exists
+if command -v zsh >/dev/null 2>&1; then
+	echo "zsh found."
+else
+	# Install zsh
+	echo "No zsh found. Installing..."
+
+	if command -v brew >/dev/null 2>&1; then
+		echo "Homebrew found. Using..."
+		brew install zsh
+	else
+		echo "Trying apt..."
+		sudo apt-get install zsh
+	fi
+fi
+
+if command -v chsh >/dev/null 2>&1; then
+	chsh -s $(which zsh)
+fi
+
+zsh
+exit
